88ba906eb22dedc25d666445ea104090ee1219c5
anvlaibar <olav@haug1.no>
2019-01-13 20:54:33 +0100

    updates to keymap and layers

--- keyboards/idobo/keymaps/anvlaibar/keymap.c
+++ keyboards/idobo/keymaps/anvlaibar/keymap.c
@@ -45,11 +45,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * '--------------------------------------------------------------------------------------------------------------------------------------'
   */
  [_QW] = LAYOUT_ortho_5x15( \
-    KC_ESC,    KC_1,      KC_2,      KC_3,      KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,       KC_0,        NO_PLUS,     NO_BSLS,     KC_TILD,     KC_BSPC, \
-    KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,       KC_P,        NO_AA,       NO_QUOT,     KC_LBRC,    KC_RBRC, \
-    NO_PIPE,   KC_A,      KC_S,      KC_D,      KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,       NO_OSLH,     NO_AE,       NO_ASTR,     KC_RSPC,    KC_ENT,  \
-    KC_LSFT,   NO_LESS,   KC_Z,      KC_X,      KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,    KC_DOT,      KC_MINUS,     KC_SCOLON,   KC_UP,      KC_DEL,  \
-    KC_LCTL,   KC_LGUI,   KC_PERC,   KC_PSLS,   KC_LALT,  DF(0),    KC_SPC,   KC_SPC,   OSL(1),   KC_RALT,    LT(1, KC_SPC),   KC_AMPR,   KC_LEFT,     KC_DOWN,    KC_RGHT  \
+    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, NO_PLUS, NO_BSLS, KC_TILD, KC_BSPC, \
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, NO_AA, NO_QUOT, KC_LBRC, KC_RBRC, \
+    NO_PIPE, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, NO_OSLH, NO_AE, NO_ASTR, KC_RSPC, KC_ENT, \
+    KC_LSFT, NO_LESS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_MINUS, KC_SCOLON, KC_UP, KC_DEL, \
+    KC_LCTL, KC_LGUI, KC_PERC, KC_PSLS, KC_LALT, DF(0), KC_SPC, KC_SPC, OSL(1), KC_RALT, LT(1, KC_SPC), KC_AMPR, KC_LEFT, KC_DOWN, KC_RGHT \
   ),
 
 	/* Function & Media keys - LAYER 1
@@ -66,11 +66,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* '-------------------------------------------------------------------------------------------------------------------------------------'
 	*/
  	[_FN] = LAYOUT_ortho_5x15(
-  	XXXXXXX,    KC_F1,      KC_F2,			   KC_F3,    		KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,    KC_F11,     KC_F12,     KC_F13,    XXXXXXX, \
-  	KC_LEAD,    KC_MPLY,    KC_MSTP,    	   KC_MPRV,    		KC_MNXT,    KC_MUTE,    KC_VOLD,    KC_VOLU,    XXXXXXX,    KC_PSCR,      KC_SLCK,   KC_PAUS,    KC_INS,    XXXXXXX,    KC_CALC, \
-  	KC_LOCK,    XXXXXXX,    XXXXXXX, XXXXXXX,  XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, \
-  	XXXXXXX,    XXXXXXX, XXXXXXX,    XXXXXXX,  		   XXXXXXX,  		XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, \
-  	RESET,    XXXXXXX,    XXXXXXX,  		   XXXXXXX,  		XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX  \
+  	XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_F13, XXXXXXX, \
+  	KC_LEAD, KC_MPLY, KC_MSTP, KC_MPRV, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, KC_PSCR, KC_SLCK, KC_PAUS, KC_INS, XXXXXXX, KC_CALC, \
+  	KC_LOCK, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  	XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  	RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX \
 	),
 
    /* RGB-controls - LAYER 2
@@ -87,11 +87,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* '-------------------------------------------------------------------------------------------------------------------------------------'
 	*/
  	[_RGB] = LAYOUT_ortho_5x15(
-  	XXXXXXX,    XXXXXXX,      XXXXXXX,			   XXXXXXX,    		XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,    XXXXXXX,     XXXXXXX,     XXXXXXX,    XXXXXXX, \
-  	XXXXXXX,    XXXXXXX,    XXXXXXX,    	   XXXXXXX,    		XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,      XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, \
-  	XXXXXXX,    BL_TOGG,    BL_STEP,    BL_INC,    BL_DEC,    BL_BRTG,    XXXXXXX,    XXXXXXX,   XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX, XXXXXXX,XXXXXXX,XXXXXXX\
-  	XXXXXXX,    RGB_TOG, RGB_VAI,    RGB_VAD,  		   RGB_HUI,  		RGB_HUD,    RGB_SAI,    RGB_SAD,    RGB_MOD,    RGB_RMOD, RGB_M_R,  RGB_M_P,   RGB_M_SW,    RGB_M_X,   RGB_M_K,  \
-  	XXXXXXX,    XXXXXXX,    XXXXXXX,  		   XXXXXXX,  		XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,   XXXXXXX,    RGB_M_T,    RGB_M_G,    RGB_M_SN  \
+  	XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  	XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  	XXXXXXX, BL_TOGG, BL_STEP, BL_INC, BL_DEC, BL_BRTG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  	XXXXXXX, RGB_TOG, RGB_VAI, RGB_VAD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_MOD, RGB_RMOD, RGB_M_R, RGB_M_P, RGB_M_SW, RGB_M_X, RGB_M_K, \
+  	XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_M_T, RGB_M_G, RGB_M_SN \
 	),
 };
 
@@ -123,7 +123,7 @@ void matrix_scan_user(void) {
 }
 
 
-/* Layer change light hint */
+/* Detect layer change and change light */
 uint32_t layer_state_set_user(uint32_t state) {
     switch (biton32(state)) {
     case _RAISE:
