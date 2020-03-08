c5824741cfb6fec3ec720ff0a039998aa4f49edf
hauola17 <hauola17@student.kristiania.no>
2020-03-08 21:55:24 +0100

    raw hid working

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/anvlaibar/keymap.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/anvlaibar/keymap.c
@@ -14,10 +14,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+
 #include "keymap_norwegian.h"
 
 // Add headers for raw hid communication
-//#include <split_scomm.h>
 #include "raw_hid.h"
 
 /* 	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, K012, K013, K212, K014, \
@@ -32,21 +32,25 @@ extern uint8_t is_master;
 // Aliases
 #define ___ KC_TRNS
 
+// Layer defines
+#define _BASE 0
+#define _RAISE 1
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_all( /* Base */
+  [_BASE] = LAYOUT_all( /* Base */
 // ╭──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────╮
    GRAVE_ESC,KC_1,  KC_2,   KC_3,  KC_4,   KC_5,   KC_6,   KC_7,  KC_8,   KC_9,  KC_0, KC_MINS, KC_EQL,KC_BSPC,KC_DEL,KC_HOME,
 // ├──────┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴──────┼──────┤
       KC_TAB,    KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,  KC_Y,   KC_U,  KC_I,   KC_O,   KC_P, KC_LBRC,KC_RBRC, KC_BSLS, KC_PGUP,
 // ├──────────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┬────┴─┐       ├──────┤
-      KC_GRV,     KC_A,   KC_S,  KC_D,   KC_F,   KC_G,  KC_H,   KC_J,   KC_K,  KC_L, KC_SCLN,KC_QUOT,/*NO_APOS,*/KC_ENT,KC_PGDN,
+      KC_NUBS,     KC_A,   KC_S,  KC_D,   KC_F,   KC_G,  KC_H,   KC_J,   KC_K,  KC_L, KC_SCLN,KC_QUOT,/*NO_APOS,*/KC_ENT,KC_PGDN,
 // ├────────┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴──┬───┴┬─────┴─┬────┴──────┴┬──────┼──────┤
-  	KC_LSFT, KC_NUBS,  KC_Z,  KC_X,   KC_C,  KC_V,   KC_B,   KC_N,  KC_M, KC_COMM,KC_DOT,KC_SLSH,    KC_RSFT,   KC_UP,  KC_END,
+  	KC_LSFT, KC_GRV,  KC_Z,  KC_X,   KC_C,  KC_V,   KC_B,   KC_N,  KC_M, KC_COMM,KC_DOT,KC_SLSH,    KC_RSFT,   KC_UP,  KC_END,
 // ├────────┼──────┴─┬────┴───┬──┴──────┴──────┴──────┴──────┴──────┴──┬──┴─────┼───────┴┐╭───╮┌─────┼──────┼──────┤
 	   KC_LCTL, KC_LALT,   KC_LGUI,    KC_SPC,         KC_SPC,         KC_SPC,    KC_RALT,  OSL(1),      KC_LEFT,KC_DOWN,KC_RGHT
 // ╰────────┴────────┴────────┴────────────────────────────────────────┴────────┴─────────╯   ╰──────┴──────┴──────╯
 ),
-[1] = LAYOUT_all( /* FN */
+[_RAISE] = LAYOUT_all( /* FN */
 // ╭──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────╮
      RESET, KC_F1,  KC_F2, KC_F3,  KC_F4,  KC_F5, KC_F6,  KC_F7, KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,  ___,   ___,  KC_MPLY, //16
 // ├──────┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴───┬──┴──────┼──────┤
@@ -62,26 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // Raw hid variables
-uint8_t layer_index = 0
-
-// TODO: Implement layer change code
-// https://docs.qmk.fm/#/custom_quantum_functions?id=layer-change-code
-layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
-    case 0:
-       // rgblight_setrgb (0x00,  0x00, 0xFF);
-        break;
-    case 1:
-        //rgblight_setrgb (0xFF,  0x00, 0x00);
-        break;
-    default: //  for any other layers, or the default layer
-        //rgblight_setrgb (0x00,  0xFF, 0xFF);
-        break;
-    }
-  return state;
-}
-
-
+uint8_t layer_index = 0;
 
 
 // TODO: Implement Raw HID, to be able to send layer state to host
@@ -90,4 +75,27 @@ void raw_hid_send_layer_index(void) {
   uint8_t send_data[32] = {0};
   send_data[0] = layer_index + 1; // Add one so that we can distinguish it from a null byte
   raw_hid_send(send_data, sizeof(send_data));
-}
+};
+// TODO: Implement layer change code
+// https://docs.qmk.fm/#/custom_quantum_functions?id=layer-change-code
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case _BASE:
+        layer_index = 0;
+        raw_hid_send_layer_index();
+        break;
+    case _RAISE:
+        layer_index = 1;
+        raw_hid_send_layer_index();
+        break;
+    default: //  for any other layers, or the default layer
+        layer_index = 0;
+        raw_hid_send_layer_index();
+        break;
+    }
+  return state;
+};
+
+
+
+
